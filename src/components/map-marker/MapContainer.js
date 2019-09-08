aaee29d7ef0f5ee9f4e87c95ee20f6a9ca40d8b9
Manish <mani.2209@gmail.com>
2019-09-08 11:07:59 +0530

    remove marker

--- src/components/map-marker/MapContainer.js
+++ src/components/map-marker/MapContainer.js
@@ -2,16 +2,47 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import Geosuggest from 'react-geosuggest';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
 
 export class MapContainer extends Component {
     constructor(props) {
       super(props);
   
       this.state = {
-        stores: [{placeId: 'ChIJOyH6ZBmEsUcRD',label: 'Old Elbe Tunnel, Hamburg', latitude: 53.5495629, longitude: 9.9625838}]
+        stores: [{placeId: 'ChIJOyH6ZBmEsUcRD',label: 'Old Elbe Tunnel, Hamburg', latitude: 53.5495629, longitude: 9.9625838}],
+        checkCountry: true,
       }
 
       this.onSuggestSelect = this.onSuggestSelect.bind(this)
+      this.remove = this.remove.bind(this)
+    }
+
+    remove(placeId){
+      console.log('remove', placeId)
+      const idToRemove = placeId
+      const filterLocation = this.state.stores.filter((item) => item.placeId !== idToRemove)
+
+      console.log(filterLocation)
+      this.setState({
+        stores: filterLocation
+      })
     }
   
     displayMarkers = () => {
@@ -31,6 +62,29 @@ export class MapContainer extends Component {
       if(suggest === undefined) return
       let stores = this.state.stores
 
+      // Quota exhausted
+      // let geoUrl = `http://api.geonames.org/countryCodeJSON?lat=${suggest.location.lat}&lng=${suggest.location.lng}&username=demo`
+
+      // fetch(geoUrl)
+      //   .then((res) => {
+      //     console.log(res)
+      //     if(res.countryName !== 'Germany'){
+      //       this.setState({ checkCountry: false })
+      //     }else {
+      //       stores.push({
+      //         placeId: suggest.placeId,
+      //         label: suggest.label,
+      //         latitude: suggest.location.lat,
+      //         longitude: suggest.location.lng
+      //       })
+      
+      //       this.setState({
+      //         stores: stores
+      //       })
+      //       console.log(stores)
+      //     }
+      //   })
+
       stores.push({
         placeId: suggest.placeId,
         label: suggest.label,
@@ -42,6 +96,8 @@ export class MapContainer extends Component {
         stores: stores
       })
       console.log(stores)
+
+     
     }
   
     render() {
@@ -75,16 +131,37 @@ export class MapContainer extends Component {
               placeholder="Start typing!"
               initialValue=""
               fixtures={fixtures}
+              onClick={() => this.setState({ checkCountry: true })}
               onSuggestSelect={this.onSuggestSelect}
               location={new google.maps.LatLng(53.558572, 9.9278215)}
               radius="20" />
               <button onClick={()=>this._geoSuggest.clear()}>Clear</button>
+              {this.state.checkCountry === false ? <p>The marker is outside Germany</p>: ''}
               <h3>Added locations: </h3>
-              <ul>
-                {this.state.stores  ? this.state.stores.map((item, index) => {
-                  return <li key={index}>{item.label}</li>
-                }) : ''} 
-              </ul>
+              <Paper className={''}>
+                <Table className={'classes.table'}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Location</TableCell>
+                      <TableCell align="right">Lattitude</TableCell>
+                      <TableCell align="right">longitude</TableCell>
+                      <TableCell align="right">Action</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {this.state.stores.map((item, index) => (
+                      <TableRow key={index}>
+                        <TableCell component="th" scope="row">
+                          {item.label}
+                        </TableCell>
+                        <TableCell align="right">{item.latitude}</TableCell>
+                        <TableCell align="right">{item.longitude}</TableCell>
+                        <TableCell align="right"><button onClick={() => this.remove(item.placeId)}>Delete</button></TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
             </Grid>
             
           </Grid>
